1c825997ba8813c700b1efd0f47ee0164af818b0
yeePlayz <sverreabo@gmail.com>
2022-04-24 11:50:03 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,75 @@
+<script>
+	import Nav from "./Nav.svelte";
+	export let name;
+</script>
+
+<main>
+	<div class="container">
+		<div class="relative">
+			<div class="Nav">
+				<Nav />
+			</div>
+			<div class="relative">
+				<img id="logo-img" src="logo.png" />
+				<img id="prikk-img" src="prikk.png" />
+			</div>
+		</div>
+
+		<div class="flex flex-space-between">
+			<h1>Tønsberg</h1>
+			<h1>dato</h1>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 1024px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		margin: 0;
+	}
+
+	.container {
+		width: min(80%, 512px);
+		margin: 0 auto;
+	}
+
+	.relative {
+		position: relative;
+	}
+
+	#logo-img {
+		width: 100%;
+		height: auto;
+	}
+
+	#prikk-img {
+		position: absolute;
+		top: 17.5%;
+		left: 100.5%;
+		width: calc(min(80%, 512px) * 0.12);
+	}
+
+	.Nav {
+		z-index: 1;
+		position: absolute;
+		top: 35%;
+		left: 34%;
+	}
+
+	h1 {
+		color: rgb(35, 31, 32);
+		font-size: 3rem;
+		letter-spacing: -1px;
+	}
+
+	@media (max-width: 640px) {
+		.Nav {
+			position: static;
+		}
+	}
+</style>
---
+++ src/Nav.svelte
@@ -0,0 +1,36 @@
+<main>
+	<nav>
+		<button href="Om festivalen">Om festivalen</button>
+
+		<button>Program</button>
+
+		<button>Kjøp billetter</button>
+
+		<button>Kontakt</button>
+	</nav>
+</main>
+
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+	}
+
+	nav {
+		display: flex;
+		flex-direction: column;
+	}
+
+	button {
+		text-align: left;
+		background: none;
+		border: none;
+		font-size: 20px;
+		color: rgb(255, 255, 255);
+	}
+
+	button:hover,
+	button:focus-visible {
+		background-color: rgb(130, 194, 117);
+	}
+</style>

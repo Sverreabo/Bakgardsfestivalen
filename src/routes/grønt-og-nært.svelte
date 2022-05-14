3dbf66e41d4a15cd1028d64d4f254b4650392bc3
yeePlayz <sverreabo@gmail.com>
2022-05-14 07:36:36 +0200

    Grønt og nært

--- src/routes/grønt-og-nært.svelte
+++ src/routes/grønt-og-nært.svelte
@@ -2,16 +2,84 @@
 	<title>Grønt og nært</title>
 </svelte:head>
 
-<section>
-	<h1>Grønt og nært</h1>
-	<p>
-		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius natus atque ipsam dicta, iure ut
-		laudantium illo culpa nostrum dignissimos obcaecati architecto blanditiis reprehenderit suscipit
-		ullam libero molestias nihil deleniti. Eligendi facere, dignissimos at aliquam odit velit
-		pariatur iusto ipsum modi dolores illum recusandae hic eos, explicabo, similique sapiente
-		aspernatur. Facilis officia cupiditate deserunt nihil maxime ipsum eligendi repellat ea. Saepe
-		facere mollitia molestias inventore impedit! At, a ab culpa, voluptate ea quisquam voluptas
-		doloremque soluta esse quo porro cum distinctio laborum! Nobis, excepturi atque voluptas
-		corporis quis veritatis a!
-	</p>
-</section>
+<main>
+	<div class="relative">
+		<div class="over-img">
+			<h2>Grønt og nært</h2>
+		</div>
+		<div class="container-wide">
+			<img id="img-geitmyra" width="1634" height="1080" alt="" src="/Geitmyra.jpg" />
+		</div>
+		<div class="over-img" id="text-over-img">
+			<p>
+				I bakgården, med inngangen ved siden av Tønsberg Optiske i Kammegaten 5, blir det grønn
+				matkultur. Det er Geitmyra matkultursenter som står for organisering og innhold. De skriver
+				om seg selv på sin nettside: Vi jobber for å pirre nysgjerrighet, dyrke kunnskap og øke
+				matgleden til barna som kommer til oss. Og vi vet at barn kan lage mat, de tørr å smake og
+				at de må tas på alvor. Når barna lærer å ta gode matvalg gir vi dem kunnskap de kan dra
+				nytte av gjennom hele livet. Det er dette som er ideen med å lage en egen grønn bakgård
+				under festivalen.
+			</p>
+		</div>
+	</div>
+
+	<div class="container">
+		<p>Aktiviteter blir:</p>
+		<ul>
+			<li>Smak en plante/spis en blomst</li>
+			<li>Lage frøbomber Sjekk den belgen!</li>
+			<li>Smak påbelgfrukt, så ertespirer</li>
+		</ul>
+		<p>
+			Vi håper også å kunne grille sesongens grønnsaker, hvis sikkerheten tillater det på denne
+			tørre tiden av året.
+		</p>
+		<div class="flex">
+			<p>
+				Uansett kommer <a href="https://biatilpia.no/" target="_blank">Bia til Pia</a> fra Sandefjord.
+				Hun formidler kunnskap om bier, birøkt og den viktige rollen biene har i vårt økosystem. Dette
+				formidles til barna gjennom lek, underholdning og workshop. Det blir dermed lettere å sette pris
+				på og ta vare på biene våre som er en viktig pollinator.
+			</p>
+			<img id="img-bia" width="955" height="955" src="/bia til pia.png" />
+		</div>
+	</div>
+</main>
+
+<style>
+	.over-img {
+		position: absolute;
+		width: 100%;
+		background-color: rgba(155, 204, 145, 0.5);
+	}
+
+	.over-img > * {
+		margin-left: calc((100vw - var(--container-width)) / 2);
+		width: var(--container-width);
+	}
+
+	#text-over-img {
+		bottom: 0;
+		background-color: rgba(35, 31, 32, 0.7);
+		color: white;
+		font-size: 1.2rem;
+	}
+
+	#img-geitmyra {
+		width: 100%;
+		height: auto;
+	}
+
+	#img-bia {
+		width: 25%;
+		height: min-content;
+	}
+
+	@media (max-width: 640px) {
+		#text-over-img {
+			position: relative;
+			background-color: initial;
+			color: initial;
+		}
+	}
+</style>

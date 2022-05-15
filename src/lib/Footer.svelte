2b588965dfbc8339425f4fdad9d9627b73ed8299
yeePlayz <sverreabo@gmail.com>
2022-05-15 10:55:10 +0200

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,5 +1,5 @@
 <main>
-	<div class="container flex flex-space-between">
+	<div class="container flex flex-space-between flex-wrap">
 		<p>Siden er laget av Sverre</p>
 		<p>Mer info? feks. kontakt eller annet</p>
 	</div>
@@ -9,5 +9,9 @@
 	main {
 		background-color: rgb(35, 31, 32);
 		color: #ffff;
+	}
+
+	p {
+		margin: 1rem 0.5rem;
 	}
 </style>

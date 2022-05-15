0726061f824152fb739c034c01b297e943f69a98
yeePlayz <sverreabo@gmail.com>
2022-05-15 09:41:33 +0200

    Program

---
+++ src/routes/program/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Nav from '$lib/Nav.svelte';
+</script>
+
+<main class="container">
+	<Nav>
+		<a href="/program">Program</a>
+		<a href="/program/forfatterene">Forfatterene</a>
+	</Nav>
+	<slot />
+</main>

907c435dc2298ff97455c0de9568205687806ce4
yeePlayz <sverreabo@gmail.com>
2022-06-10 13:39:03 +0200

    Update scenekunst.svelte

--- src/routes/program/barneprogram/scenekunst.svelte
+++ src/routes/program/barneprogram/scenekunst.svelte
@@ -43,7 +43,7 @@
 			<li>
 				<h3>Tjøme Litterære Forum presenterer:</h3>
 				<ul>
-					<li>13:00: <i>Høna med bæsj i ryggesekken </i></li>
+					<li>13:00: <i>Den lille høna med bæsj i ryggsekken</i></li>
 					<li>13:30: <i>Grevlingsanger</i> med Even Tømte</li>
 				</ul>
 			</li>

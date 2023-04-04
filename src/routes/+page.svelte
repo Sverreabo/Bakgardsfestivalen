6de5cc1f5b0a17f41a21982e70b4e57ba80c4699
Sverre <sverreabo@gmail.com>
2023-04-04 10:43:08 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,16 @@
 </svelte:head>
 
 <main class="container">
-	<h2>Ønsker du å bidra?</h2>
+	<h2>Sponsorer og samarbeidspartnere</h2>
+	<p>Uten sponsorer. Ingen festival.</p>
 	<p>
-		Send en epost til <a href="mailto:karlsen.lisbet@gmail.com"
-			>karlsen.lisbet@gmail.com</a
-		> eller ring oss på 92252647.
+		Så viktig er sponsorene for Bakgårdsfestivalen. Uten midler og velvilje
+		blir det ikke fest. For oss som har påtatt oss ansvaret for at det blir
+		liv i bakgårdene i år også, gir det trygghet å vite at vi har byen i
+		ryggen. Næringslivet støtter festivalen, det gjør kommunen og det
+		offentlige også. En gratis festival er ikke billig. Vi i K7 og alle som
+		bidrar frivillig for gleden og fellesskapets skyld slik at vi kan vise
+		noe av hva kulturbyen Tønsberg kan by på, har sponsorene å takke. Så på
+		vegne av oss alle – tusen takk!
 	</p>
-	<p>Mer info kommer!</p>
 </main>

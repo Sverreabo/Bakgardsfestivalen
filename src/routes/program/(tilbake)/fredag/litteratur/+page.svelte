c4d7f16c517d0e752bfb6b42fdcd75b907339301
yeePlayz <sverreabo@gmail.com>
2023-04-25 17:08:53 +0200

    Foreløpig 25.04

--- src/routes/program/(tilbake)/fredag/litteratur/+page.svelte
+++ src/routes/program/(tilbake)/fredag/litteratur/+page.svelte
@@ -1,7 +1,50 @@
+<script>
+    import "$lib/program.css";
+</script>
+
 <svelte:head>
     <title>Litteratur på fredag</title>
 </svelte:head>
 
 <main class="container">
-    <h3>Litteratur</h3>
+    <div class="program">
+        <h3>15:00</h3>
+        <ul>
+            <li>
+                <p>
+                    <i>Skjærgård, sommer og proseccokrim</i>.
+                    <a href="/program/info/#Dorthe-Erischsen">Dorthe Erichsen</a
+                    >
+                    og
+                    <a href="/program/info/#Lene-Lauritzen-Kjølner"
+                        >Lene Lauritzen Kjølner</a
+                    > i samtale med Audhild Lønne.
+                </p>
+            </li>
+        </ul>
+    </div>
+    <div class="program">
+        <h3>16:00</h3>
+        <ul>
+            <li>
+                <p>
+                    <a href="/program/fredag/litteratur/info/#Kvinnelinjer>"
+                        >Kvinnelinjer</a
+                    >. Kvinners livsfortellinger. (Foreningen KULT v/ Norunn
+                    Askeland og Lisbeth Thoresen)
+                </p>
+            </li>
+        </ul>
+    </div>
 </main>
+
+<style>
+    a {
+        text-decoration: underline;
+        color: black;
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/program/(tilbake)/fredag/litteratur/info/+page.svelte
@@ -0,0 +1,44 @@
+<svelte:head>
+    <title>Info til program</title>
+</svelte:head>
+
+<main class="container">
+    <h2>Info til program</h2>
+    <div id="Dorthe-Erichsen">
+        <p>
+            <span class="navn">Dorthe Erichsen</span> er bosatt i Drammen. Hun er
+            utdannet cand.mag., har tidligere arbeidet som forlagsredaktør og jobber
+            nå som oversetter og serieromanforfatter. Hun har skrevet tre serier:
+            SOMMERLAND, LOSBARNA og SETERLIV. I de to første er handlingen lagt til
+            Tjøme, mens SETERLIV foregår i Hallingdal. I denne samtalen skal det
+            dreie seg om Sommerland og Tjøme.
+        </p>
+    </div>
+    <div id="Lene-Lauritzen-Kjølner">
+        <p>
+            <span class="navn">Lene Lauritzen Kjølner</span> er bosatt i Tønsberg.
+            Hun har skrevet åtte bøker om detektiven Olivia Henriksen utgitt på Fagervik
+            forlag. Hun sier selv at hun skriver i feelgood-sjangeren – og at det
+            dreier seg om prosecco- skjærgårds- og hyggekrim. Pluss litt til.
+        </p>
+    </div>
+    <div id="Kvinnelinjer">
+        <p>
+            <span class="navn">Kvinnelinjer</span> er en bok med tekster av kvinner
+            fra ulike kulturer, inkludert Norge. På arrangementet vil disse forfatterne
+            lese egne livsfortellinger: Cristina Almeida, Solveig Tinderholt, Yenni
+            Nguyen, Camilla Victoria Aubert Hellern, Kari Plyhn og Kjersti Wold.
+        </p>
+    </div>
+</main>
+
+<style>
+    .navn {
+        text-decoration: underline;
+        font-size: 1.2rem;
+    }
+
+    p {
+        margin-bottom: 1.5rem;
+    }
+</style>

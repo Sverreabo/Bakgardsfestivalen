20fe9b34161d59ae889515b2df74ce4fb7343d87
yeePlayz <sverreabo@gmail.com>
2023-05-10 18:34:11 +0200

    Barneprogram 10.05

--- src/routes/barneprogram/+page.svelte
+++ src/routes/barneprogram/+page.svelte
@@ -7,41 +7,20 @@
 </svelte:head>
 
 <main>
-	<!-- <div class="relative">
-		<div class="over-img">
-			<h2 class="over-img-container">Grønn matkultur</h2>
-		</div>
-		<div class="container-wide">
-			<picture>
-				<source
-					media="(min-width: 1080px)"
-					srcset="/Geitmyra_short.jpg"
-				/>
-				<img
-					id="img-geitmyra"
-					width="1634"
-					height="1080"
-					alt=""
-					src="/Geitmyra.jpg"
-				/>
-			</picture>
-		</div>
-		<div class="over-img text-over-img">
-			<p class="over-img-container">
-				I bakgården, med inngangen ved siden av Tønsberg Optiske i
-				Kammegaten 5, blir det grønn matkultur. Det er Geitmyra
-				matkultursenter som står for organisering og innhold. De skriver
-				om seg selv på sin nettside: Vi jobber for å pirre
-				nysgjerrighet, dyrke kunnskap og øke matgleden til barna som
-				kommer til oss. Og vi vet at barn kan lage mat, de tørr å smake
-				og at de må tas på alvor. Når barna lærer å ta gode matvalg gir
-				vi dem kunnskap de kan dra nytte av gjennom hele livet. Det er
-				dette som er ideen med å lage en egen grønn bakgård under
-				festivalen.
-			</p>
-		</div>
-	</div> -->
-
+	<div class="container container-fill-small">
+		<img
+			src="/barneprogram-fredag.png"
+			width="1640"
+			height="924"
+			alt="Barneprogram fredag"
+		/>
+		<img
+			src="/barneprogram-lørdag.png"
+			width="1640"
+			height="924"
+			alt="Barneprogram lørdag"
+		/>
+	</div>
 	<div class="container">
 		<div class="program">
 			<h2>Fredag 11:00 - 14:00</h2>

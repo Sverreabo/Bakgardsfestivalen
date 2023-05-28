939cf7c06bf9954bd7c0ae9476f5b91b18720613
Sverre <sverreabo@gmail.com>
2023-05-28 08:12:55 +0200

    Barneprogram fiks

--- src/routes/barneprogram/+page.svelte
+++ src/routes/barneprogram/+page.svelte
@@ -22,198 +22,123 @@
 		/>
 	</div>
 	<div class="container program">
-		<h2>Fredag 9. juni - Sentrumsnære barnehager er spesielt invitert</h2>
-		<ul>
-			<li>
-				<h3>Kl. 11:00</h3>
-				<p>
-					Osebergkatta leder barnehagene til riktig bakgård. Alle
-					barnehagene møter på torvet og møter Osebergkatta
-				</p>
-				<ul>
-					<li>
-						<p>
-							Barnehagene går etter Osebergkatta etter oppsatte
-							grupper
-						</p>
-					</li>
-				</ul>
-			</li>
+		<section>
+			<h2>Fredag 9. juni</h2>
+			<p>Lukket program for spesielt inviterte barnehager</p>
+		</section>
 
-			<li>
-				<h3>Kl. 11:30</h3>
-				<p>
-					Aktiviteten varer ca 30 min. Deretter slår barnehagen følge
-					med Osebergkatta til neste bakgård
-				</p>
-				<ul>
-					<li>
-						<p>
-							Bakgård for Grønn matkultur i Kammegaten:
-							<b
-								>Geitmyra matkultursenter og lokale aktører gir
-								barna en «grønn» opplevelse</b
-							>
-						</p>
-					</li>
-					<li>
-						<p>
-							Bakgård for Litteratur i Øvre Langgate: <b
-								>Tønsberg og Nøtterøy bibliotek gir barna en
-								samlingsstund rundt boka
-							</b>
-						</p>
-						<figure>
+		<section class="program">
+			<h2>Lørdag 10. juni - Grønn gate</h2>
+			<p>Ulike stasjoner med fokus på matkultur og bærekraft</p>
+			<ul>
+				<li>
+					<h3>Kl. 11:00</h3>
+					<ul>
+						<li>
+							<p>
+								Musikkscenen på Friluften: <b
+									>Politimester Bastian og Tønsberg Byorkester
+									åpner dagen</b
+								>
+							</p>
+						</li>
+					</ul>
+				</li>
+
+				<li>
+					<h3>Kl. 11:30 - 14:00</h3>
+					<ul>
+						<li>
+							<p>
+								Grønn gate i Kammegaten fokus på grønn matkultur
+								og bærekraft:
+								<b
+									>Ulike aktører gir barna en «grønn»
+									opplevelse hele dagen</b
+								>
+							</p>
+						</li>
+						<li>
+							<p>
+								Jungelbokområdet ved musikkscenen på Friluften
+								åpner: <b
+									>Barna får fargelegge motiver fra Jungelboka
+									hele dagen</b
+								>
+							</p>
+						</li>
+					</ul>
+				</li>
+
+				<li>
+					<h3>Kl. 11:30</h3>
+					<ul>
+						<li>
+							<p>
+								Bakgård for Litteratur i Øvre Langgate: <b
+									>Kai og Christer lager improteater for barn
+									og unge</b
+								>
+							</p>
 							<img
 								class="p60"
-								src="/biblioteket.jpg"
-								width="605"
-								height="806"
-								alt="To bibliotekarer"
+								src="/improteater.jpg"
+								width="1049"
+								height="1398"
+								alt="Kai og Christer"
 							/>
+						</li>
+					</ul>
+				</li>
 
-							<figcaption>
-								<i>
-									Det var en gang to bibliotekarer som skulle
-									lese høyt for barnehagebarna under
-									Bakgårdsfestivalen. På veien til bakgården
-									møtte de på en hemmelighetsfull bjørn og et
-									sultent troll. Det er deres historier
-									bibliotekarene forteller på scenen. Her blir
-									det både latter og spenning for store og
-									små.
-								</i>
-							</figcaption>
-						</figure>
-					</li>
-					<li>
-						<p>
-							Bakgården for Musikk og aktiviteter i Torvgaten: <b
-								>Kattas Figurteater gir barna en kreativ
-								opplevelse</b
-							>
-						</p>
-					</li>
-				</ul>
-			</li>
+				<li>
+					<h3>Kl. 12:30</h3>
+					<ul>
+						<li>
+							<p>
+								Bakgård for Litteratur i Øvre Langgate: <b
+									>Kuturskolen og Steinerskolen underholder</b
+								>
+							</p>
+						</li>
+					</ul>
+				</li>
 
-			<li>
-				<h3>Kl. 12:30</h3>
-				<p>Rullering til neste bakgård</p>
-				<ul>
-					<li>
-						<p>
-							Osebergkatta leder barnehagene videre til riktig
-							bakgård. Samme program i bakgårdene
-						</p>
-					</li>
-				</ul>
-			</li>
+				<li>
+					<h3>Kl. 13:00</h3>
+					<ul>
+						<li>
+							<p>
+								Musikkscenen på Friluften: <b
+									>Tønsberg Amatørteater framfører utdrag fra
+									Jungelboka. Etter forestillingen kan barn få
+									ansiktsmaling med motiver fra Jungelboka</b
+								>
+							</p>
+							<img
+								src="/Jungelboken.jpg"
+								width="640"
+								height="401"
+								alt="Tønsberg Amatørteater"
+							/>
+						</li>
+					</ul>
+				</li>
 
-			<li>
-				<h3>Kl. 13:30</h3>
-				<p>Rullering til neste bakgård</p>
-			</li>
-
-			<li>
-				<h3>Kl. 14:00</h3>
-				<p>Slutt på programmet - takk for i dag!</p>
-			</li>
-		</ul>
-
-		<h2>Lørdag 10. juni - Åpen familiedag</h2>
-		<ul>
-			<li>
-				<h3>Kl. 11:00</h3>
-				<p>Korpsmusikk på torvet åpner dagen</p>
-			</li>
-
-			<li>
-				<h3>Hele dagen (Kl. 11:30 - 14:00)</h3>
-				<ul>
-					<li>
-						<p>
-							Bakgård for Grønn matkultur i Kammegaten: <b
-								>Geitmyra matkultursenter og lokale aktører gir
-								barna en «grønn» opplevelse hele dagen</b
-							>
-						</p>
-					</li>
-					<li>
-						<p>
-							Bakgården for Musikk og aktiviteter i Torvgaten: <b
-								>Barna får kreative opplevelse og aktivitet hele
-								dagen
-							</b>
-						</p>
-					</li>
-				</ul>
-			</li>
-
-			<li>
-				<h3>Kl. 11:30</h3>
-				<ul>
-					<li>
-						<p>
-							Bakgård for Litteratur i Øvre Langgate: <b
-								>Kai og Christer lager improteater for barn og
-								unge</b
-							>
-						</p>
-						<img
-							class="p60"
-							src="/improteater.jpg"
-							width="1049"
-							height="1398"
-							alt="Kai og Christer"
-						/>
-					</li>
-				</ul>
-			</li>
-
-			<li>
-				<h3>Kl. 12:30</h3>
-				<ul>
-					<li>
-						<p>
-							Bakgård for Litteratur i Øvre Langgate: <b
-								>Utdrag av Jungelboka av Tønsberg Amatørteater
-							</b>
-						</p>
-						<img
-							src="/Jungelboken.jpg"
-							width="640"
-							height="401"
-							alt="Tønsberg Amatørteater"
-						/>
-					</li>
-				</ul>
-			</li>
-			<li>
-				<h3>Kl. 13:30</h3>
-				<ul>
-					<li>
-						<p>
-							Bakgård for Litteratur i Øvre Langgate: <b
-								>Kai og Christer lager improteater for barn og
-								unge</b
-							>
-						</p>
-						<img
-							class="p60"
-							src="/improteater2.jpg"
-							width="749"
-							height="998"
-							alt="Kai og Christer"
-						/>
-					</li>
-				</ul>
-			</li>
-
-			<li>
-				<h3>Kl. 14:00</h3>
-				<p>Slutt på programmet - takk for i dag!</p>
-			</li>
-		</ul>
+				<li>
+					<h3>Kl. 13:30</h3>
+					<ul>
+						<li>
+							<p>
+								Fra torvet til Friluften: <b
+									>Færder Musikkorps avslutter barne- og
+									familieprogrammet</b
+								>
+							</p>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</section>
 	</div>
 </main>
